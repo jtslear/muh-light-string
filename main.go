82183ace326ef3b323a4a46d426b489efafa2a53
John T Skarbek <jtslear@gmail.com>
2017-12-23 18:42:58 -0500

    Makes the lights work with the web app
    * extends the app such that it can consume a context that contains the
    RGB color config

--- main.go
+++ main.go
@@ -1,14 +1,32 @@
 package main
 
 import (
-	"io"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/lucasb-eyer/go-colorful"
 	"gobot.io/x/gobot/drivers/gpio"
 	"gobot.io/x/gobot/platforms/raspi"
 )
+
+type appContext struct {
+	c *gpio.RgbLedDriver
+}
+
+type appHandler struct {
+	*appContext
+	h func(*appContext, http.ResponseWriter, *http.Request) (int, error)
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
 
 var indexHTML = `
 <html>
@@ -25,10 +43,27 @@ var indexHTML = `
 </html>
 `
 
-func testFunction(w http.ResponseWriter, r *http.Request) {
+func (ah appHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	status, err := ah.h(ah.appContext, w, r)
+	if err != nil {
+		log.Printf("HTTP %d: %q", status, err)
+		switch status {
+		case http.StatusNotFound:
+			http.NotFound(w, r)
+		case http.StatusInternalServerError:
+			http.Error(w, http.StatusText(status), status)
+		default:
+			http.Error(w, http.StatusText(status), status)
+		}
+	}
+}
+
+func color(a *appContext, w http.ResponseWriter, r *http.Request) (int, error) {
+	log.Printf("%s %s\n", r.Method, r.URL.Path)
 	switch r.Method {
 	case http.MethodGet:
-		io.WriteString(w, indexHTML)
+		fmt.Fprintf(w, indexHTML)
+		return 200, nil
 	case http.MethodPost:
 		err := r.ParseForm()
 		if err != nil {
@@ -41,41 +76,47 @@ func testFunction(w http.ResponseWriter, r *http.Request) {
 					log.Printf("Unable to grasp the hex color: %v\n", err)
 				}
 				log.Printf("Color: R:%d G:%d B:%d\n", int(c.R*255), int(c.G*255), int(c.B*255))
+				err = a.c.SetRGB(byte(c.R*255), byte(c.G*255), byte(c.B*255))
+				check(err)
 				w.WriteHeader(http.StatusAccepted)
-			} else {
-				w.WriteHeader(http.StatusBadRequest)
+				return 202, nil
 			}
+			w.WriteHeader(http.StatusBadRequest)
+			return 400, nil
 		}
-		io.WriteString(w, indexHtml)
+		fmt.Fprintf(w, indexHTML)
+		return 200, nil
 	default:
 		w.WriteHeader(http.StatusMethodNotAllowed)
-		io.WriteString(w, indexHtml)
+		fmt.Fprintf(w, indexHTML)
+		return 200, nil
 	}
-	log.Printf("%s %s\n", r.Method, r.URL.Path)
 }
 
 func main() {
 	a := raspi.NewAdaptor()
 	// These are the PHYSICAL pins here...
+	// physical pin 12 == GPIO 4
+	// physical pin 11 == GPIO 17
+	// physical pin 7 == GPIO 18
 	roy := gpio.NewRgbLedDriver(a, "12", "11", "7")
-	if roy.State() {
-		log.Printf("It would appear on\n")
-	} else {
-		log.Printf("It is not on\n")
-	}
-	err := roy.On()
-	if err != nil {
-		log.Printf("Problem turning on the LED's: %v\n", err)
-		panic(err)
-	}
-	if roy.State() {
-		log.Printf("It would appear on\n")
-	} else {
-		log.Printf("It is not on\n")
-	}
-	http.HandleFunc("/", testFunction)
+	c := make(chan os.Signal, 2)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		err := roy.SetRGB(0, 0, 0)
+		check(err)
+		err = roy.Off()
+		check(err)
+		os.Exit(0)
+	}()
+
+	context := &appContext{roy}
+
+	http.Handle("/", appHandler{context, color})
 	port := "6060"
 
 	log.Printf("Listening on port %s\n", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
+
 }
